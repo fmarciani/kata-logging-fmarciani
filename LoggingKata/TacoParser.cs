dab9dc1eccc8296d95b532235c34362e78327c45
Francesca Marciani <fmarciani@gmail.com>
2018-04-22 22:09:37 -0500

    Added code to try-catch loop to pass out-of-numeric-range unit tests.

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -12,30 +12,38 @@ namespace LoggingKata
         
         public ITrackable Parse(string line)
         {
-            Logger.LogInfo("Begin parsing ...");
+            Logger.LogInfo("Begin parsing");
             
             // Read line, split it by comma into string array.
             var cells = line.Split(',');
 
             // If the length of the array < 3, return null.
-            if (cells.Length <= 3)
+            if (cells.Length < 3)
             {
                 Logger.LogError("Line entry has fewer than 3 cells.");
                 return null;
             }
 
             // Location name should be in cell index 2. Parse lat and lon as doubles.
+            Logger.LogInfo("Parsing location");
             var name = cells[2];
+
             double lon;
             double lat;
 
             try
             {
-                Logger.LogInfo("Parsing longitude ...");
+                Logger.LogInfo("Parsing longitude");
                 lon = double.Parse(cells[0]);
 
-                Logger.LogInfo("Parsing latitude ...");
+                Logger.LogInfo("Parsing latitude");
                 lat = double.Parse(cells[1]);
+
+                if (Math.Abs(lon) > 180 || Math.Abs(lat) > 90)
+                {
+                    Logger.LogError("Longitude or latitude out of range.");
+                    return null;
+                }
             }
             catch (Exception)
             {
@@ -43,7 +51,7 @@ namespace LoggingKata
                 return null;
             }
 
-            var tacoBell = new TacoBell() { Location = new Point(), Name = name };
+            var tacoBell = new TacoBell { Location = new Point { Longitude = lon, Latitude = lat }, Name = name };
 
             return tacoBell;
         }
