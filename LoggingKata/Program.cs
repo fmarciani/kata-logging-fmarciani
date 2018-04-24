c6e370a5fd31cfec97426ed7a3093ce876b59643
Francesca Marciani <fmarciani@gmail.com>
2018-04-23 22:00:26 -0500

    Made requested changes to code.

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -7,7 +7,6 @@ namespace LoggingKata
 {
     class Program
     {
-        //Why do you think we use ILog?
         static readonly ILog Logger = new TacoLogger();
 
         const string csvPath = "TacoBell-US-AL.csv";
@@ -33,43 +32,36 @@ namespace LoggingKata
                     break;
             }
 
-            // Create new instance of the TacoParser class.
             var parser = new TacoParser();
 
-            // Grab IEnumerable of locations.
-            var locations = lines.Select(line => parser.Parse(line)).ToArray();
+            // IEnumerable of locations.
+            var locations = lines.Select(line => parser.Parse(line));
 
-            // Create two ITrackable variables to store Taco Bell locations.
+            // Two Taco Bell locations.
             ITrackable locA = null;
             ITrackable locB = null;
-
-            // Create a variable to store the distance between two Taco Bell locations.
             double distance = 0.00;
 
-            // For each location in locations: 
             foreach (var loc1 in locations)
             {
-                // Grab each location as the origin; store the latitude and longitude in coordinate variable.
+                // Store lat and long in coordinate variables for origin and destination Taco Bells.
                 var corA = new GeoCoordinate
                 {
                     Longitude = loc1.Location.Longitude,
                     Latitude = loc1.Location.Latitude
                 };
-                // Within this loop:
+
                 foreach (var loc2 in locations)
                 {
-                    // Grab another location as the destination; store the latitude and longitude in coordinate variable.
                     var corB = new GeoCoordinate
                     {
                         Longitude = loc2.Location.Longitude,
                         Latitude = loc2.Location.Latitude
                     };
                     
-                    // Make a new variable to store the distance between the origin and the destination point.
+                    // Distance between origin and destination point.
                     var dist = corA.GetDistanceTo(corB);
 
-                    /* If the distance between the origin and destination is greater than the var distance outside the
-                     scope, adjust the var distance. Also adjust the coordinate variables outside of the scope. */
                     if (dist > distance)
                     {
                         locA = loc1;
@@ -78,9 +70,7 @@ namespace LoggingKata
                     }
                 }
             }
-
-            // Print out the two Taco Bells farthest apart from one another in the .csv file, and the distance in meters.
-            Console.WriteLine($"The largest distance in the data is between {locA.Name} and {locB.Name}, at {distance} meters.");
+            Console.WriteLine($"The largest distance in the data is between\n\t{locA.Name}\n\t{locB.Name}\nat {Math.Round(distance * 0.000621371)} miles.");
             Console.ReadLine();
         }
     }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -13,33 +13,24 @@ namespace LoggingKata
         public ITrackable Parse(string line)
         {
             Logger.LogInfo("Begin parsing");
-            
-            // Read line, split it by comma into string array.
-            var cells = line.Split(',');
 
-            // If the line is empty or the length of the array != 3, return null.
             if (string.IsNullOrEmpty(line))
             {
                 Logger.LogError("Empty line detected.");
                 return null;
             }
-            else if (cells.Length != 3)
+
+            var cells = line.Split(',');
+
+            if (cells.Length < 2)
             {
-                Logger.LogError("Line entry has fewer than 3 cells.");
+                Logger.LogError("Input failed to parse.");
                 return null;
             }
 
-            // Location name should be in cell index 2. Parse lat and lon as doubles.
             Logger.LogInfo("Parsing location");
 
             var name = cells[2];
-
-            /*
-             // Remove all characters in location cell beginning with char "(".
-             // UNSURE WHY THIS DOESN'T WORK; FAILS MULTIPLE TESTS: 
-             var uneditedName = cells[2];
-             var name = uneditedName.Remove(uneditedName.IndexOf('('));
-            */
 
             double lon;
             double lat;
@@ -57,15 +48,15 @@ namespace LoggingKata
                     Logger.LogError("Longitude or latitude out of range.");
                     return null;
                 }
+
+                var tacoBell = new TacoBell {Location = new Point {Longitude = lon, Latitude = lat}, Name = name};
+                return tacoBell;
             }
             catch (Exception e)
             {
                 Logger.LogError("Could not parse coordinates.", e);
                 return null;
             }
-
-            var tacoBell = new TacoBell { Location = new Point { Longitude = lon, Latitude = lat }, Name = name };
-            return tacoBell;
         }
     }
 }
