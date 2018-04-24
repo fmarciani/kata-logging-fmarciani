ca5eb01d28c4faffb02d6aa86b519980b6a15670
Francesca Marciani <fmarciani@gmail.com>
2018-04-24 09:38:55 -0500

    Added MaxLat and MaxLong to Point, refactored, commented out trivial logging statmenets.

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -9,10 +9,10 @@ namespace LoggingKata
     public class TacoParser
     {
         public readonly ILog Logger = new TacoLogger();
-        
+
         public ITrackable Parse(string line)
         {
-            Logger.LogInfo("Begin parsing");
+            // Logger.LogInfo("Begin parsing");
 
             if (string.IsNullOrEmpty(line))
             {
@@ -28,25 +28,27 @@ namespace LoggingKata
                 return null;
             }
 
-            Logger.LogInfo("Parsing location");
+            // Logger.LogInfo("Parsing location");
 
-            var name = cells[2];
+            var name = cells.Length > 2 ? cells[2] : "";
+            var promoIndex = name.IndexOf('(');
+            name = promoIndex > 0 ? name.Remove(promoIndex) : name;
 
             try
             {
-                Logger.LogInfo("Parsing longitude");
+                // Logger.LogInfo("Parsing longitude");
                 var lon = double.Parse(cells[0]);
 
-                Logger.LogInfo("Parsing latitude");
+                // Logger.LogInfo("Parsing latitude");
                 var lat = double.Parse(cells[1]);
 
-                if (Math.Abs(lon) > 180 || Math.Abs(lat) > 90)
+                if (Math.Abs(lon) > Point.MaxLong || Math.Abs(lat) > Point.MaxLat)
                 {
                     Logger.LogError("Longitude or latitude out of range.");
                     return null;
                 }
 
-                var point = new Point {Longitude = lon, Latitude = lat};
+                var point = new Point { Longitude = lon, Latitude = lat };
                 return new TacoBell { Location = point, Name = name };
             }
             catch (Exception e)
